9410c20db7f777feb85335814e4832deff93e065
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-12 14:34:57 -0700

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes=False)
 def state_list():
     return render_template("7-states_list.html",
                            data=storage.all(State).values())
@@ -20,4 +20,4 @@ def storage_close(var=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0' port=5000)
